5acd405f7935fcebf2a2e75951c5cd3ba3f93fc1
Lieberman, Yuval <Yuval.Lieberman@2bprecisehealth.com>
2019-08-14 07:56:13 +0300

    fix field name

--- java/data-ingestion/src/main/java/com/tobprecise/demo/bolts/RecordScheme.java
+++ java/data-ingestion/src/main/java/com/tobprecise/demo/bolts/RecordScheme.java
@@ -4,6 +4,6 @@ public class RecordScheme {
 	public final static String CONTEXT_ID = "context-id";
 	public final static String FILE_METADATA = "file-metadata";
 	public final static String RECORD = "record";
-	public final static String REASON = "record";
+	public final static String REASON = "reason";
 	public final static String VALUE = "value";
 }
